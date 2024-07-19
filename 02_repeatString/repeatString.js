51b4d2d857294c96a0713b2f6366c124f1f273de
boornimavar <boornimavarna@gmail.com>
2024-07-19 16:02:15 +0530

    Update

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
-};
+const repeatString = (word, number) => {
+    if (number < 0) return 'ERROR'; 
+  
+    let result = ''; 
+    for (let i = 0; i < number; i++) {
+      result += word; 
+    }
+    return result;
+  };
+  
+  module.exports = repeatString;
+  
 
 // Do not edit below this line
 module.exports = repeatString;
